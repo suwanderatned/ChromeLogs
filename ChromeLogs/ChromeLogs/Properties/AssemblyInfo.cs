391f29b7205ca03ad83d3fc5b0e08fb0de99c48d
suwanderatned <58134301+suwanderatned@users.noreply.github.com>
2019-11-24 21:09:54 +1100

    ChomeConsole V2

---
+++ ChromeLogs/ChromeLogs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChromeLogs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChromeLogs")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d378d270-2a3a-4cd4-aade-2c8a71a62429")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChromeLogs/ChromeLogs/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
+using ChromeLogger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace ChromeLogs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.SetLoggingPreference(LogType.Browser, LogLevel.Warning);
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www-au.computershare.com/Employee/Login/CompanyIntroduction.aspx?lang=en&issuerid=SCAUCPU&cc=AU");
+            var logs = driver.Manage().Logs;
+            var log1=driver.Manage().GetType();
+
+            var logsS = driver.Manage().Logs.GetLog(LogType.Browser);
+
+            foreach (var log in logsS)
+            {
+                Console.WriteLine(log.ToString());
+            }
+
+        }
+
+
+
+
+
+        }
+}
